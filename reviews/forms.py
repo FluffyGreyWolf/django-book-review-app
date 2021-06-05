30f816f3b9d8cddd00ac181eeb0a7f937329ce23
GreyWolf <szymonstrozynskiii@gmail.com>
2021-06-05 10:23:54 +0200

    Added review adding functionality

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,8 +1,20 @@
 from django import forms
 from django.forms.fields import CharField, ChoiceField
+from .models import Publisher, Review
 
 CHOICES = (("title", "Title"), ("contributor", "Contributor"))
 
 class SearchForm(forms.Form):
     search = CharField(required=False, min_length=3)
     search_in = ChoiceField(required=False, choices=CHOICES)
+
+class PublisherForm(forms.ModelForm):
+    class Meta:
+        model = Publisher
+        fields = "__all__"
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        exclude = ["date_editied", "book"]
+        rating = forms.IntegerField(min_value=0, max_value=5)
