80b9c83025054eed7f91d1927dcc9ea2cf4bbf2a
GreyWolf <szymonstrozynskiii@gmail.com>
2021-06-21 16:12:32 +0200

    update: custom logout page for admin panel

--- bookrAdmin/admin.py
+++ bookrAdmin/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 class BookrAdmin(admin.AdminSite):
     site_header = "Bookr Administration"
-
+    logout_template = "admin/logout.html"
 
 
 # Register your models here.
